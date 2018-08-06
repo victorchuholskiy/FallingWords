b26529c4e8abd7a83470d45ea442f419758ff8be
viktor.chukholskiy <viktor.chukholskiy@cactussoft.biz>
2018-08-06 19:53:12 +0300

    Animation

--- app/src/main/java/com/gmail/victorchuholskiy/languagegame/main/MainFragment.kt
+++ app/src/main/java/com/gmail/victorchuholskiy/languagegame/main/MainFragment.kt
@@ -6,6 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gmail.victorchuholskiy.languagegame.R
+import android.view.animation.TranslateAnimation
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_main.*
+import android.util.DisplayMetrics
+import android.util.Log
 
 /**
  * Created by victor.chuholskiy
@@ -15,11 +20,31 @@ class MainFragment : Fragment(), MainContract.View {
 
 	override lateinit var presenter: MainContract.Presenter
 
+	private var gameAreaHeight: Int = 0
+
 	override fun onCreateView(inflater: LayoutInflater,
 							  container: ViewGroup?,
 							  savedInstanceState: Bundle?): View? {
 		val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+		val gameAreaView = view.findViewById<View>(R.id.v_game_area)
+		gameAreaView.viewTreeObserver.addOnGlobalLayoutListener {
+			gameAreaHeight = gameAreaView.height
+		}
+
+		view.findViewById<TextView>(R.id.tv_question).text = String.format(getString(R.string.is_translation_correct), "test")
 		return view
+	}
+
+	override fun onResume() {
+		super.onResume()
+		startAnim()
+	}
+
+	fun startAnim() {
+		val animation = TranslateAnimation(0f,0f,0f, gameAreaHeight.toFloat())
+		animation.duration = 6000
+		tv_translation.startAnimation(animation)
 	}
 
 	companion object {
